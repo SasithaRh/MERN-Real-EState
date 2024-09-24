802a872ee48efd856903dba8bce6f09f0692e1b7
SasithaRh <ruvishansth1030@gmail.com>
2024-09-24 18:58:13 +0530

    connected to the mongoDB

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
+ import mongoose from 'mongoose';
+ import dotenv from 'dotenv';
 // import userRouter from './routes/user.route.js';
 // import authRouter from './routes/auth.route.js';
-// dotenv.config();
+ dotenv.config();
 
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => {
-//     console.log('Connected to MongoDB!');
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
