00e6daac9ba955377d0fa08eafaee77a040f9f2e
SasithaRh <ruvishansth1030@gmail.com>
2024-09-27 10:00:58 +0530

    Completed signup & signin with all functionality and add redux toolkit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
-import SignUp from './pages/Signup';
+import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
@@ -9,8 +9,9 @@ import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
+        <Header />
       <Routes>
+    
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
